2a5c8358661b28a55cc21d5b121c32a4d7948abd
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-04-03 09:06:20 +0700

    Add {data, post method}

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
+'use strict'
+
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const PORT = 3000
+const bears = [
+  { id: 1, name: 'Pooh', weight: 100 },
+  { id: 2, name: 'Winnie', weight: 200 }
+]
 
 app
-  .get('/', (req, res) => {
-    res.send('hello world')
+  .get('/api/bears', (req, res) => {
+    res.send(bears)
   })
+  .post('/api/bears', (res, res) => {
+    const bear = {}
+    bear.id = 3
+    bear.name = 'FooBear'
+    bear.weight = 88
+    bear.push(bear)
+  })
+
+app
   .listen(PORT, () => {
     console.log('listen on', PORT)
   })
